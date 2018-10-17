73ebfab1f2ebd1bbf798452e63cd3d2af1930791
Unsourced <43748934+CoderXYZ1@users.noreply.github.com>
2018-10-17 12:45:41 -0700

    Source :)

---
+++ Get IP [Console]/Get_IP_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net;
+
+namespace Get_IP_Console
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var client = new WebClient())
+            {
+                var htmlCode = client.DownloadString("https://www.myexternalip.com/raw");
+                Console.WriteLine(htmlCode);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
